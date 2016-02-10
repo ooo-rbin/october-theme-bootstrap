e70a8e9a1501a834d63ff9cb63878e460aff7f6a
Yaroslav <lar-dragon@yandex.ru>
2016-02-10 17:00:16 +0500

    Сборка с использованием Bower

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,23 @@ require('es6-promise').polyfill();
 var fs           = require('fs');
 var path         = require('path');
 var gulp         = require('gulp');
+var bower        = require('gulp-bower');
 var sass         = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 var minify_css   = require('gulp-minify-css');
 var concat       = require('gulp-concat');
 var uglify       = require('gulp-uglify');
-var minify_html  = require('gulp-minify-html');
 var prepend      = require('gulp-insert').prepend;
 var replace      = require('gulp-replace');
 var rename       = require("gulp-rename");
 
 gulp.task('default', ['styles', 'scripts', 'templates']);
-gulp.task('default:watch', ['styles:watch', 'scripts:watch', 'templates:watch']);
+gulp.task('default:watch', ['default', 'styles:watch', 'scripts:watch', 'templates:watch'], function () {
+	gulp.watch(['.bowerrc', 'bower.json'], ['bower']);
+});
+
+gulp.task('bower', function () {
+	return bower({cmd: 'update'});
+});
 
 // Стили
 
@@ -29,18 +34,18 @@ gulp.task('project-styles', function () {
 	return gulp.src([
 			'assets/styles/theme.scss'
 		])
-		.pipe(sass())
+		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('style.min.css'))
 		.pipe(minify_css())
 		.pipe(gulp.dest('assets'));
 });
 
-gulp.task('vendor-styles', ['vendor-media'], function () {
+gulp.task('vendor-styles', function () {
 	return gulp.src([
 			'assets/styles/bootstrap.scss',
 			'assets/styles/theme.scss'
 		])
-		.pipe(sass())
+		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('vendor.style.min.css'))
 		.pipe(minify_css())
 		.pipe(gulp.dest('assets'));
@@ -62,26 +67,15 @@ gulp.task('project-scripts', function () {
 		.pipe(gulp.dest('assets'));
 });
 
-gulp.task('vendor-scripts', ['vendor-media'], function () {
+gulp.task('vendor-scripts', ['bower'], function () {
 	return gulp.src([
-			'node_modules/jquery/dist/jquery.js',
-			'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+			'assets/vendor.components/jquery/dist/jquery.js',
+			'assets/vendor.components/bootstrap-sass/assets/javascripts/bootstrap.js',
 			'assets/scripts/theme.js'
 		])
 		.pipe(concat('vendor.script.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('assets'));
-});
-
-// Медиа
-
-gulp.task('vendor-media', ['vendor-fonts']);
-
-gulp.task('vendor-fonts', function () {
-	return gulp.src([
-			'node_modules/bootstrap-sass/assets/fonts/bootstrap/*'
-		])
-		.pipe(gulp.dest('assets/vendor.fonts'));
 });
 
 // Шпблоны
