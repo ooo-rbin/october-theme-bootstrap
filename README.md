703fbe2ed306378cd381531b3ab70c3e3931448c
Yaroslav <lar-dragon@yandex.ru>
2016-02-11 12:05:36 +0500

    Стили темы в LESS, автоматический поиск JS поставщиков

--- README.md
+++ README.md
@@ -4,7 +4,20 @@
 Она предназначена для CMS October.
 
 Сборка темы осуществляется командой `npm install`.
+Артифактами сборки являются:
+
+* `assets/style.min.css` - минифицированный стиль темы, полученный из компиляци `assets/styles/theme.less`;
+* `assets/vendor.style.min.css` - минифицированный стиль темы, полученный из последовательной компиляции `assets/styles/bootstrap.less` и `assets/styles/theme.less`;
+* `assets/script.min.js` - минифицированный скрипт темы, полученный последовательной сборкой из файлов вида `assets/scripts/**/*.js`;
+* `assets/vendor.script.min.js` - минифицированный скрипт темы, полученный последовательной сборкой из файлов `*.js` указанных в секции `main` устанавливаемых пакетов Bower и файлов вида `assets/scripts/**/*.js`;
+* файлы типа `*.htm` в `content/static-pages`, `layouts`, `pages`, `partials` собранные из файлов в одноименных каталогах:
+	1) `template.twig` - шаблон разметки HTML;
+	2) `functions.php` - PHP функции шаблона;
+	3) `config.ini` - параметры шаблона.
 
 Модификации темы отслеживаются командой `gulp default:watch`.
+
+Скрипты темы подключаются из `assets/scripts`.
+Основной стиль темы 'assets/styles/theme.less'.
 
 Лицензия MIT.
--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var fs           = require('fs');
 var path         = require('path');
 var gulp         = require('gulp');
 var bower        = require('gulp-bower');
-var sass         = require('gulp-sass');
+var less         = require('gulp-less');
 var minify_css   = require('gulp-minify-css');
 var concat       = require('gulp-concat');
 var uglify       = require('gulp-uglify');
@@ -14,14 +14,39 @@ var prepend      = require('gulp-insert').prepend;
 var replace      = require('gulp-replace');
 var rename       = require("gulp-rename");
 
+function wrapPipe(taskFn) {
+	return function(done) {
+		var onSuccess = function () {
+			done();
+		};
+		var onError = function (err) {
+			done(err);
+		};
+		var check = function (stream) {
+			if (stream && typeof stream.on === 'function') {
+				stream.on('end', onSuccess);
+			} else if (stream instanceof Array) {
+				stream.map(check);
+			} else {
+				throw new Error();
+			}
+		};
+		try {
+			check(taskFn(onError));
+		} catch (err) {
+			onError(err);
+		}
+	}
+}
+
 gulp.task('default', ['styles', 'scripts', 'templates']);
 gulp.task('default:watch', ['default', 'styles:watch', 'scripts:watch', 'templates:watch'], function () {
 	gulp.watch(['.bowerrc', 'bower.json'], ['bower']);
 });
 
-gulp.task('bower', function () {
+gulp.task('bower', wrapPipe(function () {
 	return bower({cmd: 'update'});
-});
+}));
 
 // Стили
 
@@ -30,26 +55,26 @@ gulp.task('styles:watch', function () {
 	gulp.watch('assets/styles/**/*.{sass,scss,css}', ['styles']);
 });
 
-gulp.task('project-styles', function () {
+gulp.task('project-styles', wrapPipe(function (err) {
 	return gulp.src([
-			'assets/styles/theme.scss'
+			'assets/styles/theme.less'
 		])
-		.pipe(sass().on('error', sass.logError))
-		.pipe(concat('style.min.css'))
-		.pipe(minify_css())
-		.pipe(gulp.dest('assets'));
-});
+		.pipe(less().on('error', err))
+		.pipe(concat('style.min.css').on('error', err))
+		.pipe(minify_css().on('error', err))
+		.pipe(gulp.dest('assets').on('error', err));
+}));
 
-gulp.task('vendor-styles', function () {
+gulp.task('vendor-styles', wrapPipe(function (err) {
 	return gulp.src([
-			'assets/styles/bootstrap.scss',
-			'assets/styles/theme.scss'
+			'assets/styles/bootstrap.less',
+			'assets/styles/theme.less'
 		])
-		.pipe(sass().on('error', sass.logError))
-		.pipe(concat('vendor.style.min.css'))
-		.pipe(minify_css())
-		.pipe(gulp.dest('assets'));
-});
+		.pipe(less().on('error', err))
+		.pipe(concat('vendor.style.min.css').on('error', err))
+		.pipe(minify_css().on('error', err))
+		.pipe(gulp.dest('assets').on('error', err));
+}));
 
 // Скрипты
 
@@ -58,32 +83,46 @@ gulp.task('styles:watch', function () {
 	gulp.watch('assets/scripts/**/*.js', ['scripts']);
 });
 
-gulp.task('project-scripts', function () {
+gulp.task('project-scripts', wrapPipe(function (err) {
 	return gulp.src([
-			'assets/scripts/theme.js'
+			'assets/scripts/**/*.js'
 		])
-		.pipe(concat('script.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('assets'));
-});
+		.pipe(concat('script.min.js').on('error', err))
+		.pipe(uglify().on('error', err))
+		.pipe(gulp.dest('assets').on('error', err));
+}));
 
-gulp.task('vendor-scripts', ['bower'], function () {
-	return gulp.src([
-			'assets/vendor.components/jquery/dist/jquery.js',
-			'assets/vendor.components/bootstrap-sass/assets/javascripts/bootstrap.js',
-			'assets/scripts/theme.js'
-		])
-		.pipe(concat('vendor.script.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('assets'));
-});
+gulp.task('vendor-scripts', ['bower'], wrapPipe(function (err) {
+	var directory = JSON.parse(fs.readFileSync('.bowerrc', 'utf8')).directory;
+	var dependencies = JSON.parse(fs.readFileSync('bower.json', 'utf8')).dependencies;
+	var libs = [];
+	var is_lib = /\.js$/i;
+	var pushLib = function (dir, lib) {
+		if (lib instanceof Array) {
+			lib.map(function (lib) {
+				pushLib(dir, lib);
+			});
+		} else if (lib.search(is_lib) >= 0) {
+			libs.push(path.join(dir, lib));
+		}
+	};
+	Object.keys(dependencies).map(function (pack) {
+		pushLib(path.join(directory, pack), JSON.parse(fs.readFileSync(path.join(directory, pack, 'bower.json'), 'utf8')).main);
+	});
+	libs.push('assets/scripts/**/*.js');
+	console.log(libs);
+	return gulp.src(libs)
+		.pipe(concat('vendor.script.min.js').on('error', err))
+		.pipe(uglify().on('error', err))
+		.pipe(gulp.dest('assets').on('error', err));
+}));
 
 // Шпблоны
 
-gulp.task('templates', function () {
-	var getFolders = function(dir) {
+gulp.task('templates', function (err) {
+	var getFolders = function (dir) {
 		return fs.readdirSync(dir)
-			.filter(function(file) {
+			.filter(function (file) {
 				return fs.statSync(path.join(dir, file)).isDirectory();
 			})
 			.map(function (subDir) {
@@ -104,20 +143,19 @@ gulp.task('templates', function () {
 		var php = path.join(folder.path, 'functions.php');
 		var ini = path.join(folder.path, 'config.ini');
 		var task = gulp.src(twig)
-			//.pipe(minify_html())
-			.pipe(prepend('==\n'));
+			.pipe(prepend('==\n').on('error', err));
 		if (fs.existsSync(php)) {
 			task
-				.pipe(prepend(fs.readFileSync(php)))
-				.pipe(replace('<?php\n', '==\n'));
+				.pipe(prepend(fs.readFileSync(php)).on('error', err))
+				.pipe(replace('<?php\n', '==\n').on('error', err));
 		}
 		if (fs.existsSync(ini)) {
 			task
-				.pipe(prepend(fs.readFileSync(ini)));
+				.pipe(prepend(fs.readFileSync(ini)).on('error', err));
 		}
 		return task
-			.pipe(rename(folder.name + '.htm'))
-			.pipe(gulp.dest(folder.base));
+			.pipe(rename(folder.name + '.htm').on('error', err))
+			.pipe(gulp.dest(folder.base).on('error', err));
 	});
 });
 gulp.task('templates:watch', function () {
